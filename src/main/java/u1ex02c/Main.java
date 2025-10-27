e193007c1e5a7d077ddbb7d0f3e055d93ea86df1
Roberto Fernández del Barrio <rfernandez3@nigul.cide.es>
2025-10-27 10:36:17 +0100

    Class Car (starting the code)

--- src/main/java/u1ex02c/Main.java
+++ src/main/java/u1ex02c/Main.java
@@ -1,7 +1,50 @@
 package u1ex02c;
 
+import javax.xml.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    @XmlRootElement
+    public static class Car {
+        private String brand;
+        private String model;
+        private int year;
+        private double price;
+
+        public Car() {
+        }
+
+        @XmlElement
+        public String getBrand() {
+            return brand;
+        }
+
+        public void setBrand(String brand) {
+            this.brand = brand;
+        }
+
+        @XmlElement
+        public String getModel() {
+            return model;
+        }
+
+        public void setModel(String model) {
+            this.model = model;
+        }
+
+        @XmlElement
+        public int getYear() {
+            return year;
+        }
+
+        public void setYear(int year) {
+            this.year = year;
+        }
+        @XmlElement
+        public double getPrice(){
+            return price;
+        }
+        public void setPrice(double price){
+            this.price = price;
+        }
     }
 }
