53566d2d663db042d4b7355d8c0b0d6464f83917
Roberto Fernández del Barrio <rfernandez3@nigul.cide.es>
2025-10-27 10:46:46 +0100

    Fixing....

--- src/main/java/u1ex02c/Main.java
+++ src/main/java/u1ex02c/Main.java
@@ -1,70 +1,107 @@
 package u1ex02c;
 
 import java.io.File;
-import java.rmi.UnmarshalException;
+import java.io.FileWriter;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
-import javax.xml.*;
+import javax.xml.bind.annotation.*;
+import javax.xml.bind.*;
 
-public class Main {
-    @XmlRootElement
-    public static class Car {
-        private String brand;
-        private String model;
-        private int year;
-        private double price;
+@XmlRootElement(name = "cars")
+class Cars {
+    private List<Car> cars;
 
-        public Car() {
-        }
-
-        @XmlElement
-        public String getBrand() {
-            return brand;
-        }
-
-        public void setBrand(String brand) {
-            this.brand = brand;
-        }
-
-        @XmlElement
-        public String getModel() {
-            return model;
-        }
-
-        public void setModel(String model) {
-            this.model = model;
-        }
-
-        @XmlElement
-        public int getYear() {
-            return year;
-        }
-
-        public void setYear(int year) {
-            this.year = year;
-        }
-
-        @XmlElement
-        public double getPrice() {
-            return price;
-        }
-
-        public void setPrice(double price) {
-            this.price = price;
-        }
+    @XmlElement(name = "car")
+    public List<Car> getCars() {
+        return cars;
     }
 
+    public void setCars(List<Car> cars) {
+        this.cars = cars;
+    }
+}
+
+@XmlType(propOrder = { "brand", "model", "year", "price" })
+class Car {
+    private String brand;
+    private String model;
+    private int year;
+    private double price;
+
+    public Car() {
+    }
+
+    @XmlElement
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    @XmlElement
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    @XmlElement
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @XmlElement
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
+
+public class Main {
     public static void main(String[] args) {
         try {
+            // Load your existing XML
             File file = new File("cars.xml");
-            JAXBContext jaxbContext = JAXBContext.newInstance(Car.class);
-            Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+            JAXBContext context = JAXBContext.newInstance(Cars.class);
+            Unmarshaller unmarshaller = context.createUnmarshaller();
 
-            Car car = (Car) unmarshaller.unmarshal(file);
-            System.out.println("Brand" + car.getBrand());
-            System.out.println("Brand" + car.getModel());
-            System.out.println("Brand" + car.getYear());
-            System.out.println("Brand" + car.getPrice());
-        } catch (JAXBException e) {
+            Cars cars = (Cars) unmarshaller.unmarshal(file);
+            List<Car> list = cars.getCars();
+
+            // Sort by year (ascending)
+            Collections.sort(list, Comparator.comparingInt(Car::getYear));
+
+            // Print to console and write to file
+            FileWriter writer = new FileWriter("cars.txt");
+
+            for (Car car : list) {
+                String output = String.format(
+                        "Brand: %s%nModel: %s%nYear: %d%nPrice: %.2f%n%n",
+                        car.getBrand(),
+                        car.getModel(),
+                        car.getYear(),
+                        car.getPrice());
+                System.out.print(output);
+                writer.write(output);
+            }
+
+            writer.close();
+            System.out.println("Results saved to cars.txt\r\n");
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
