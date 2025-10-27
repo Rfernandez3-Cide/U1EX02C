2634bd326eadb5b7e9adcc58ee2a76b3995b123a
Roberto Fernández del Barrio <rfernandez3@nigul.cide.es>
2025-10-27 10:44:42 +0100

    public static void main(String[] args) (Run and Debug)

--- src/main/java/u1ex02c/Main.java
+++ src/main/java/u1ex02c/Main.java
@@ -1,5 +1,8 @@
 package u1ex02c;
 
+import java.io.File;
+import java.rmi.UnmarshalException;
+
 import javax.xml.*;
 
 public class Main {
@@ -39,12 +42,30 @@ public class Main {
         public void setYear(int year) {
             this.year = year;
         }
+
         @XmlElement
-        public double getPrice(){
+        public double getPrice() {
             return price;
         }
-        public void setPrice(double price){
+
+        public void setPrice(double price) {
             this.price = price;
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            File file = new File("cars.xml");
+            JAXBContext jaxbContext = JAXBContext.newInstance(Car.class);
+            Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+
+            Car car = (Car) unmarshaller.unmarshal(file);
+            System.out.println("Brand" + car.getBrand());
+            System.out.println("Brand" + car.getModel());
+            System.out.println("Brand" + car.getYear());
+            System.out.println("Brand" + car.getPrice());
+        } catch (JAXBException e) {
+            e.printStackTrace();
         }
     }
 }
