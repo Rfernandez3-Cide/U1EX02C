ffdb528f511ca547c63ae73fb8ff2a2b7bd5edc1
Rfernandez3-Cide <rfernandez3@nigul.cide.es>
2025-10-27 16:12:59 +0100

    Continue fixing...

--- src/main/java/u1ex02c/Main.java
+++ src/main/java/u1ex02c/Main.java
@@ -6,9 +6,6 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-import javax.xml.bind.annotation.*;
-import javax.xml.bind.*;
-
 @XmlRootElement(name = "cars")
 class Cars {
     private List<Car> cars;
